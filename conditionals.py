29646295ca3212e97c7a4aa9bf42dc661f54b0fb
Adeyefa Oluwatoba <sainttobs@gmail.com>
2019-01-18 13:59:03 +0100

    continuing conditionals

--- conditionals.py
+++ conditionals.py
@@ -2,8 +2,9 @@
 
 print("To continue enter your name")
 name = input("Enter your name: ")
+age = input("Enter your age: ")
 
-if name.title() == "Toba":  # use != to check for not equal to
+if name.title() == "Toba" and age == 19:  # use != to check for not equal to
     print("Welcome Mr. " + name.title())
 else:
     print("No user found")
