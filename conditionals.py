ef7eb7dcd778039309315a5dca3065b9b4c5a356
Adeyefa Oluwatoba <sainttobs@gmail.com>
2019-01-18 14:15:35 +0100

    conditionals, and, or statement

--- conditionals.py
+++ conditionals.py
@@ -4,7 +4,21 @@ print("To continue enter your name")
 name = input("Enter your name: ")
 age = input("Enter your age: ")
 
-if name.title() == "Toba" and age == 19:  # use != to check for not equal to
+if name.title() == "Toba" and age == 19:  # use != to check for not equal to, using *and* statement both
+    # conditions must be true, using *or* statement only one condition needs to be true.
     print("Welcome Mr. " + name.title())
 else:
     print("No user found")
+
+# check if a value exists in a list
+
+regNames = ['toba', 'adeyefa', 'oluwatoba']
+
+username = input("Enter your name: ")
+if username in regNames:  # checking if the user already exists in the list
+    print("username already taken")
+else:
+    regNames.append(username)  # adding the new user to the list
+    print("new user added")
+    print(regNames)
+
