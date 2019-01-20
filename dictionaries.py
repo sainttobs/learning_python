626098d335cceb24bc9c838030b1627e700ccf7d
Adeyefa Oluwatoba <sainttobs@gmail.com>
2019-01-20 23:32:38 +0100

    creating dictionaries

---
+++ dictionaries.py
@@ -0,0 +1,4 @@
+# creating a dictionary. A dictionary is a collection of key vale pairs just like Objects
+book = {'author': 'Adeyefa Oluwatoba', 'price': 100}  # creating a dictionary
+print(book['author'])
+print(book['price'])
