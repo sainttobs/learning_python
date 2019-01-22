e4fe53fee847a7cda9dc110cf4bd770f034d9c2c
Adeyefa Oluwatoba <sainttobs@gmail.com>
2019-01-22 15:02:15 +0100

    adding comments to the file

--- practice.py
+++ practice.py
@@ -1,13 +1,13 @@
 ages = [10, 15, 30, 21, 17, 19, 18, 13, 14, 18, 23]  # creating  a list of ages
 
-adult = []  # an empty list for adults
+adults = []  # an empty list for adults
 children = []  # an empty list for children
 
-for age in ages:
-    if age >= 18:
-        adult.append(age)
+for age in ages:  # looping through the ages list
+    if age >= 18:  # if the age is 18 or above
+        adults.append(age)  # add the age to the adults list
     else:
-        children.append(age)
+        children.append(age)  # if it is below 18 add it to the children list
 
-print(children)
-print(adult)
+print(children)  # print the children list
+print(adults)
