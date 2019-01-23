a30fac0646b8e77e28ffaae7a57c0a763f754191
Adeyefa Oluwatoba <sainttobs@gmail.com>
2019-01-23 17:20:21 +0100

    accepting user input

--- inputs.py
+++ inputs.py
@@ -2,3 +2,7 @@
 
 welcome = input("Please input your name: ")
 print("Welcome " + welcome.title())
+
+prompt = "Who is your best fiend and why do you like him/her"
+friend = (input(prompt))
+print(friend)
