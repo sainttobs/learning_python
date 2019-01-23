4ea094e4aaf6baa25db818796330aacd5fd54108
Adeyefa Oluwatoba <sainttobs@gmail.com>
2019-01-23 10:45:43 +0100

    editing dictionaries

--- dictionaries.py
+++ dictionaries.py
@@ -1,4 +1,18 @@
 # creating a dictionary. A dictionary is a collection of key vale pairs just like Objects
 book = {'author': 'Adeyefa Oluwatoba', 'price': 100}  # creating a dictionary
+
 print(book['author'])
-print(book['price'])
+
+if 'title' in book:  # checking if a key exists in a dictionary
+    print("The key title exists in the dictionary 'book.'")
+else:
+    print("The key title does not exist in the dictionary 'book'.")
+
+book['title'] = "Freak The Mighty"  # adding a new key to a dictionary
+
+# using the .get() method to retrieve a value from a dictionary. the second parameter in the method is a default in case
+# the key does not exists in the dictionary
+del book['price']  # to delete a key from a dictionary
+print(book.get('title', 'the key "title" does not exist in the dictionary'))
+print(book.get('genre', 'the key "genre" does not exist in the dictionary'))
+print(book)
