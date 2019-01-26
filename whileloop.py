3a54f9c91bb78053f9884cd1aa043353765d8b75
Adeyefa Oluwatoba <sainttobs@gmail.com>
2019-01-26 02:15:15 +0100

    populating a dictionary with user input

--- whileloop.py
+++ whileloop.py
@@ -41,3 +41,32 @@ while unconfirmed_users:
 print("The following users have been cnfirmed")
 for confirmed_user in confirmed_users:
     print(confirmed_user.title())
+
+# use .remove("value") to remove all instances of a specific value from a list, also use a while loop to loop
+# through the list
+
+# Populating a dictionary with user inputs, a property rental program
+
+rental_properties = {}
+
+# Set a flag to indicate we taking applications
+rental_open = True
+
+while rental_open:
+    # prompt users for users name and address.
+    username = input("\nWhat is your name? ")
+    rental_property = input("What is the address of the property that you have to rent? ")
+
+    # store the responses in a dictionary
+    rental_properties[username] = rental_property
+
+    # ask if the user knows anyone else looking to rent a property
+    repeat = input("\nDo you know anyone who might like to rent out their property? ")
+    if repeat == 'no':
+        rental_open = False
+
+    # adding property is complete
+
+print("\n--- Property to rent ---")
+for username, rental_property in rental_properties.items():
+    print(username + " has " + rental_property + " to rent.")
