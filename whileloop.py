83bb98f107e16aac6983d3b8c169f76a75e674af
Adeyefa Oluwatoba <sainttobs@gmail.com>
2019-01-26 01:13:25 +0100

    using flags to end while loops

--- whileloop.py
+++ whileloop.py
@@ -3,3 +3,18 @@ num = 1
 while num <= 10:
     print(num)
     num += 1
+
+# using a flagto stop a while loop
+
+prompt = "\n press q to quit this program"
+prompt += "\n Enter your name: "
+
+active = True# set the flag to True
+
+while active:
+    message = input(prompt)
+
+    if message == "q":  # if the input message is q, quit the program
+        active = False
+    else:
+        print(message)
