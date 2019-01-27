e5df2301820d75d0e888f8ee1887343219ce1822
Adeyefa Oluwatoba <sainttobs@gmail.com>
2019-01-27 14:29:21 +0100

    functions: creating functions and passing arguments to them

---
+++ functions/create_function.py
@@ -0,0 +1,11 @@
+# create a function in python
+
+
+def add(num1, num2):  # passing an argument to a function
+    # num1 = int(input("enter first number"))  # use int() to make the input an integer
+    # num2 = int(input("enter second number"))
+    plus = num1 + num2
+    print(plus)
+
+
+add(10, 20)
