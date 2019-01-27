f94861722083e3608d725b1f2308d0ef27c64d0f
Adeyefa Oluwatoba <sainttobs@gmail.com>
2019-01-27 18:30:25 +0100

    functions

--- functions/create_function.py
+++ functions/create_function.py
@@ -4,8 +4,8 @@
 def add(num1, num2):  # passing an argument to a function
     # num1 = int(input("enter first number"))  # use int() to make the input an integer
     # num2 = int(input("enter second number"))
-    plus = num1 + num2
-    print(plus)
+    addition = num1 + num2  # add the two numbers
+    print(addition)  # print the addition
 
 
-add(10, 20)
+add(10, 20)  # 30
