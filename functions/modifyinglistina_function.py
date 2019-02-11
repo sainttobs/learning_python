93cd41a83ce244cddc01949682de6b30533a11fa
Adeyefa Oluwatoba <sainttobs@gmail.com>
2019-02-11 22:46:24 +0100

    modiffying lists in a function

--- functions/modifyinglistina_function.py
+++ functions/modifyinglistina_function.py
@@ -1,10 +1,12 @@
-# passing a list to a function
+# modifying a list in a function
+
 
 available_books = ['Freak The Mighty', 'Elon Musk', 'Purple Hibiscus', 'Half of a Yellow Sun']
 
 
 def books_available(books):
     books.append('Last days at Forcados')
+    del books[2]
     for book in books:
         print('The book ' + book + " is available")
 
