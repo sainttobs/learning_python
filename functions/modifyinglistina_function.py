7f9d4808019d7cda6940132994ffa01d39e0f19d
Adeyefa Oluwatoba <sainttobs@gmail.com>
2019-02-17 03:30:12 +0100

    making a copy of a list

--- functions/modifyinglistina_function.py
+++ functions/modifyinglistina_function.py
@@ -2,13 +2,14 @@
 
 
 available_books = ['Freak The Mighty', 'Elon Musk', 'Purple Hibiscus', 'Half of a Yellow Sun']
+use_available_books = available_books[:]  # making a copy of a list to use in the function so the original list
+# remains untampered with
 
-
-def books_available(books):
-    books.append('Last days at Forcados')
-    del books[2]
-    for book in books:
-        print('The book ' + book + " is available")
+def books_available(use_available_books):
+    use_available_books.append('Last days at Forcados')
+    del use_available_books[2]
+    for use_available_book in available_books:
+        print('The book ' + use_available_book + " is available")
 
 
 books_available(available_books)
