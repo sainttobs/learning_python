21a5b5c9e9f2ddf1954fd404c78f26cb73525a3c
Adeyefa Oluwatoba <sainttobs@gmail.com>
2019-02-23 09:12:45 +0100

    giving your function an alias name

--- functions/importspecificfuntion_function.py
+++ functions/importspecificfuntion_function.py
@@ -1,11 +1,8 @@
 # importing  specific function from a module
 
-from passinglistto_function import book_description, books_available
+from passinglistto_function import books_available as ba  # give your imported function an alias name
 
 print("This is our first function")
-books_available("Elon Musk")
+ba("Elon Musk")
 
 # you can also import more than 1 function by using a comma (,) to separate each function
-
-print("\n This is our second function")
-book_description("Chimamanda")
