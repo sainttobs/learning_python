fb9a1eea63c97979e34b9d0695cd0f742d376d97
Adeyefa Oluwatoba <sainttobs@gmail.com>
2019-02-23 09:02:53 +0100

    importing a specific function from a module

---
+++ functions/importspecificfuntion_function.py
@@ -0,0 +1,11 @@
+# importing  specific function from a module
+
+from passinglistto_function import book_description, books_available
+
+print("This is our first function")
+books_available("Elon Musk")
+
+# you can also import more than 1 function by using a comma (,) to separate each function
+
+print("\n This is our second function")
+book_description("Chimamanda")
--- functions/passinglistto_function.py
+++ functions/passinglistto_function.py
@@ -6,3 +6,8 @@ available_books = ['Freak The Mighty', 'Elon Musk', 'Purple Hibiscus', 'Half of 
 def books_available(*books):
     for book in books:
         print('The book ' + book + " is available")
+
+
+def book_description(author_name, book_type="non-fiction"):
+    print("\nThis is a " + book_type + " book")
+    print("The author of the book is " + author_name.title())
