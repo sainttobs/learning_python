329844dd4ad7a47aae9981b91ac09c1a2fea15e2
Adeyefa Oluwatoba <sainttobs@gmail.com>
2019-02-25 20:33:51 +0100

    creating multipe instances of a class

--- classes/create_class.py
+++ classes/create_class.py
@@ -20,7 +20,16 @@ class Book():
         """Simulate an ebook"""
         print(self.name.title() + " is an ebook")
 
+    def ebook_reader(self):
+        """Simulate an ebook reader"""
+        print("library: " + self.name.title() + ", " + str(self.price) + ", " + self.publisher.title() + ".")
+
 
 my_book = Book('Elon Musk', 19.99, 'Virgin Books')
+your_book = Book('Freak the Mighty', 29.99, 'Atlanta Books')
 print("I am currently reading " + my_book.name.title())
 print("This book cost $" + str(my_book.price) + " and it was published by " + my_book.publisher.title())
+
+my_book.softback()
+your_book.hardback()  # creating another instance of the book class
+my_book.ebook_reader()
