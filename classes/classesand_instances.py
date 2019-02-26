81b7c6573ec07c3dca4e349252beb2c081f5edd2
Adeyefa Oluwatoba <sainttobs@gmail.com>
2019-02-26 10:12:26 +0100

    setting default values for an attribute

--- classes/classesand_instances.py
+++ classes/classesand_instances.py
@@ -11,13 +11,19 @@ class Ereader():
         self.backlight = backlight
         self.battery = battery
         self.screen_type = screen_type
-
+        self.library_count = 0
 
     def get_ereader_name(self):
         """Return a formatted descriptive name for our ereader"""
         long_name = self.make + ' - ' + self.model + ' - ' + self.backlight + ' - ' + self.battery + ' - ' + self.screen_type
         return long_name.title()
 
+    def read_library_count(self):
+        """Show the amount of books in our Library"""
+        print("You have " + str(self.library_count) + " book in your kindle library.")
+
 
 my_new_ereader = Ereader('Amozon Kindle', 'Paperwhite', 'Adjustable Backliht', 'Several Months of Backlight','300 dpi')
+
 print(my_new_ereader.get_ereader_name())
+my_new_ereader.read_library_count()
