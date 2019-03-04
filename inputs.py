b942ac0a587b6a23c5ce2ec64c86035034b6c5af
Adeyefa Oluwatoba <sainttobs@gmail.com>
2019-03-04 20:20:26 +0100

    small changes

--- inputs.py
+++ inputs.py
@@ -5,4 +5,7 @@ print("Welcome " + welcome.title())
 
 prompt = "Who is your best fiend and why do you like him/her"
 friend = (input(prompt))
+
+age = int(input("Enter your age"))  # accepting an integer input
+print ("The square of your age is "+ str(age ** 2))  # squaring the age
 print(friend)
