2df398e7651c8ca4a8f300373994b321a76921e2
Adeyefa Oluwatoba <sainttobs@gmail.com>
2019-03-04 20:29:26 +0100

    smallchanges

--- inputs.py
+++ inputs.py
@@ -3,9 +3,9 @@
 welcome = input("Please input your name: ")
 print("Welcome " + welcome.title())
 
-prompt = "Who is your best fiend and why do you like him/her"
+prompt = "Who are you: "
 friend = (input(prompt))
 
 age = int(input("Enter your age"))  # accepting an integer input
-print ("The square of your age is "+ str(age ** 2))  # squaring the age
+print ("The square of your age is " + str(age ** 2))  # squaring the age
 print(friend)
